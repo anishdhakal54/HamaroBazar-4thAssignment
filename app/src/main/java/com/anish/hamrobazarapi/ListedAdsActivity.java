b0d9a091d016edc9cc567878af2cdec7abfb53e5
anishdhakal54 <anish.dhakal54@gmail.com>
2020-01-09 19:59:16 +0545

    All layouts added

---
+++ app/src/main/java/com/anish/hamrobazarapi/ListedAdsActivity.java
@@ -0,0 +1,14 @@
+package com.anish.hamrobazarapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListedAdsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listed_ads);
+    }
+}
