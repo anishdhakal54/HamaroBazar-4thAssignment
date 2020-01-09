62f1fccf41797f518ad28140d70f5492726da074
anishdhakal54 <anish.dhakal54@gmail.com>
2020-01-09 20:12:03 +0545

    ImageResponse and signup response added

---
+++ app/src/main/java/com/anish/hamrobazarapi/serverresponse/SignupResponse.java
@@ -0,0 +1,26 @@
+package com.anish.hamrobazarapi.serverresponse;
+
+public class SignupResponse {
+    private String status ,token;
+
+    public SignupResponse(String status, String token) {
+        this.status = status;
+        this.token = token;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
